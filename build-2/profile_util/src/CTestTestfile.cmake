44b7a5cca0cc9339f7a5d9449b2c274efef2023f
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2025-10-29 11:16:27 +0800

    Fix, added incorrect profile util directory

--- build-2/profile_util/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /software/projects/pawsey0001/pelahi/arepo_solas/profile_util/src
-# Build directory: /software/projects/pawsey0001/pelahi/arepo_solas/build-2/profile_util/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
