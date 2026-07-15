29a5fcd9248bac22359c2c930b8ce595985b0973
Nick Maragkakis <mnickolas.1@gmail.com>
2026-07-15 16:41:32 +0800

    We dont need to reset density flag at the very end

--- src/stars/star_density.c
+++ src/stars/star_density.c
@@ -364,18 +364,14 @@ void star_density(void)
 
   generic_comm_pattern(TimeBinsStar.NActiveParticles, kernel_local, kernel_imported);
 
-  myfree(StarHostDistance); myfree(StarHostTask); myfree(StarHostIndex); myfree(StarNgbs);
-
-  /* Mark as active */
-  for(i = 0; i < NumStars; i++)
-     SP[i].DensityFlag = 1;
-
   /* Sort the hosts list */
   mysort(MechanicalFeedbackEvents.MechanicalFeedbackData, MechanicalFeedbackEvents.NumEvents, 
   sizeof(Mechanical_Feedback_Data), feedback_compare);
 
   /* Find the total number of events */
   sumup_large_ints(1, &MechanicalFeedbackEvents.NumEvents, &MechanicalFeedbackEvents.TotEvents);
+
+  myfree(StarHostDistance); myfree(StarHostTask); myfree(StarHostIndex); myfree(StarNgbs);
   
   /* Collect timing information */
   TIMER_STOP(CPU_STARS_DENSITY);
